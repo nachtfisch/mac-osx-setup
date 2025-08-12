2f224dd568597feb6fe62e092db57c30d37d0aa0
Christian Fischer <ch.fish@gmail.com>
2025-08-12 11:56:55 +0200

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,5 +1,6 @@
 # install x-code (cmd tools), requires manual confirmation
 xcode-select --install
 
-# install brew (package manager)
-# install chemoi (dotfiles manager)
+# install brew (package manager), see https://brew.sh/ 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask iterm2
