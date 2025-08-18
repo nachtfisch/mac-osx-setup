308ff3d13c39a9c0a69d2c8bbda75e856d92d4d6
Christian Fischer <christian.fischer@properti.ch>
2025-08-18 18:29:45 +0200

    NOTICKET added node setup

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ brew install --cask iterm2
 brew install --cask cursor
 brew install --cask visual-studio-code
 brew install --cask camunda-modeler 
-
+brew install --cask docker
+brew install node
 
 brew install uv # python version manager
